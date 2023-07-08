951d810bc9249e415b95f4f2c84761b6f5f2a842
Girish_N <ngirish1729@gmail.com>
2023-07-08 20:07:19 +0530

    4 commit fixed the image bug

--- src/data/destinationDB.jsx
+++ src/data/destinationDB.jsx
@@ -3,7 +3,7 @@ export let data = [
     _id: 1,
     name: "The Mars Experience",
     price: 11000,
-    image: "https://girish-n7.github.io/space-tour//assets/mars_base.jpg",
+    image: "https://girish-n7.github.io/space-tour/assets/mars_base.jpg",
     status: "available",
     description:
       "Ever wonder what it's like to be a Martian? Now you can wonder what your friends are doing on Earth while you walk on Mars!",
@@ -12,7 +12,7 @@ export let data = [
     _id: 2,
     name: "Jupiter Ascending",
     price: 16700,
-    image: "https://girish-n7.github.io/space-tour//assets/jupiter_aurora.jpg",
+    image: "https://girish-n7.github.io/space-tour/assets/jupiter_aurora.jpg",
     status: "available",
     description:
       "Travel to Jupiter* to take a look at the beatiful auroras. (*view from space.)",
@@ -21,7 +21,7 @@ export let data = [
     _id: 3,
     name: "Rings of Saturn",
     price: 26500,
-    image: "https://girish-n7.github.io/space-tour//assets/saturn_rings.jpg",
+    image: "https://girish-n7.github.io/space-tour/assets/saturn_rings.jpg",
     status: "few",
     description:
       "Take a look at the breathtaking rings of Saturn while being on* Saturn! (*cannot actually land.)",
@@ -31,14 +31,14 @@ export let data = [
     name: "All aboard the ISS",
     status: "few",
     price: 7500,
-    image: "https://girish-n7.github.io/space-tour//assets/ISS_img.jpg",
+    image: "https://girish-n7.github.io/space-tour/assets/ISS_img.jpg",
     description: "Visit the International Space Station and meet the crew!",
   },
   {
     _id: 5,
     name: "Lunar Journey",
     price: 5000,
-    image: "https://girish-n7.github.io/space-tour//assets/lunar_base.jpg",
+    image: "https://girish-n7.github.io/space-tour/assets/lunar_base.jpg",
     status: "filled",
     description:
       "The ultimate budget-friendly space tour. If you want a basic tour at an inexpensive rate, this is the one.",
